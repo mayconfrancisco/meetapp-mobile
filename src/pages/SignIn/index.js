49b9695dfca7e0223a37f7d6d2bde09ac45d7c79
Maycon Francisco <mayconjf@gmail.com>
2019-10-21 09:09:09 -0300

    Added Custom InputText Component

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
 import { Container } from './styles';
 
 export default function SignIn() {
@@ -9,6 +10,8 @@ export default function SignIn() {
     <Background>
       <Container>
         <Text>SignIn</Text>
+
+        <Input icon="add" placeholder="insira um texto" />
       </Container>
     </Background>
   );
