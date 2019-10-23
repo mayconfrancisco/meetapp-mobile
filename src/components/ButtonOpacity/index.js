52f57059be211a0787433aac23766335180f6aeb
Maycon Francisco <mayconjf@gmail.com>
2019-10-23 09:24:11 -0300

    adjusted ButtonOpacity Icon

--- src/components/ButtonOpacity/index.js
+++ src/components/ButtonOpacity/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Icon } from 'native-base';
+import PropTypes from 'prop-types';
 
 import { Container, Text } from './styles';
 
-export default function ButtonOpacity({ children, ...rest }) {
+export default function ButtonOpacity({ children, icon, iconSize, ...rest }) {
   return (
     <Container {...rest}>
-      <Text>{children}</Text>
+      {icon ? (
+        <>
+          <Icon
+            type="MaterialIcons"
+            name={icon}
+            style={{ fontSize: iconSize, color: '#fff' }}
+          />
+        </>
+      ) : (
+        <Text>{children}</Text>
+      )}
     </Container>
   );
 }
+
+ButtonOpacity.propTypes = {
+  children: PropTypes.element,
+  icon: PropTypes.string,
+  iconSize: PropTypes.number,
+};
+
+ButtonOpacity.defaultProps = {
+  children: null,
+  icon: null,
+  iconSize: 20,
+};
