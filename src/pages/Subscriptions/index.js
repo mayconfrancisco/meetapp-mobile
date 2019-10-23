98f548db10be5c213e2fbe5d41c04e26db866ce8
Maycon Francisco <mayconjf@gmail.com>
2019-10-23 16:51:16 -0300

    added Subscriptions and Unsubscribe

--- src/pages/Subscriptions/index.js
+++ src/pages/Subscriptions/index.js
@@ -1,14 +1,82 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
 import { Icon } from 'native-base';
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+
+import Background from '~/components/Background';
+import Header from '~/components/Header';
+import LoadingScreen from '~/components/LoadingScreen';
+import MeetupList from '~/components/MeetupList';
+import { Container, Content } from './styles';
 
 export default function Subscriptions() {
+  const [loading, setLoading] = useState(true);
+  const [meetups, setMeetups] = useState(true);
+
+  useEffect(() => {
+    async function loadMeetups() {
+      try {
+        setLoading(true);
+        const { data } = await api.get('/subscriptions');
+
+        const dataMeetups = data.map(mtSubs => {
+          return {
+            ...mtSubs.Meetup,
+            id: mtSubs.id,
+            id_meetup: mtSubs.Meetup.id,
+            dateFormatted: format(
+              parseISO(mtSubs.Meetup.date),
+              "dd 'de' MMMM', às' HH",
+              {
+                locale: pt,
+              },
+            ),
+          };
+        });
+
+        setMeetups(dataMeetups);
+      } catch (err) {
+        Alert.alert(err.response.data.erro);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadMeetups();
+  }, []);
+
+  async function onSubscribe(id) {
+    try {
+      await api.delete(`/subscriptions/${id}`);
+      setMeetups(meetups.filter(mt => mt.id !== id));
+      Alert.alert('Inscrição cancelada com sucesso');
+    } catch (err) {
+      console.tron.error(err.response.data.erro);
+      Alert.alert(err.response.data.erro);
+    }
+  }
+
   return (
-    <View>
-      <Text>Subscriptions</Text>
-    </View>
+    <Background>
+      <Header />
+      <Container>
+        <Content>
+          {loading ? (
+            <LoadingScreen />
+          ) : (
+            <MeetupList
+              data={meetups}
+              fieldKey="id"
+              onAction={onSubscribe}
+              onActionLabel="Cancelar inscrição"
+            />
+          )}
+        </Content>
+      </Container>
+    </Background>
   );
 }
 
