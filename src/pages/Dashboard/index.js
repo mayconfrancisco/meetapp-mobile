a073437c42caa33e2268dc07493c4557141c7bf0
Maycon Francisco <mayconjf@gmail.com>
2019-10-23 11:48:16 -0300

    add DashboardMeetups Screen

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,85 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useMemo, useEffect } from 'react';
 import { Icon } from 'native-base';
+import { format, parseISO, addDays, subDays } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+
+import Background from '~/components/Background';
+import Header from '~/components/Header';
+import ButtonOpacity from '~/components/ButtonOpacity';
+import LoadingScreen from '~/components/LoadingScreen';
+import MeetupList from '~/components/MeetupList';
+import {
+  Container,
+  SelectDateContainer,
+  DateSelected,
+  Content,
+} from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+  const [loading, setLoading] = useState(true);
+  const [meetups, setMeetups] = useState(true);
+  const dateFormatted = useMemo(() => {
+    return format(date, "dd 'de' MMMM", { locale: pt });
+  }, [date]);
+
+  useEffect(() => {
+    async function loadMeetups() {
+      try {
+        setLoading(true);
+        const { data } = await api.get('/meetups', {
+          // params: { date },
+        });
+
+        const dataMeetups = data.map(mt => {
+          return {
+            ...mt,
+            dateFormatted: format(parseISO(mt.date), "dd 'de' MMMM', às' HH", {
+              locale: pt,
+            }),
+          };
+        });
+
+        setMeetups(dataMeetups);
+      } catch (err) {
+        console.tron.console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadMeetups();
+  }, [date]);
+
   return (
-    <View>
-      <Text>Dashboard</Text>
-    </View>
+    <Background>
+      <Header />
+      <Container>
+        <SelectDateContainer>
+          <ButtonOpacity
+            onPress={() => setDate(subDays(date, 1))}
+            icon="chevron-left"
+            iconSize={40}
+          />
+          <DateSelected>{dateFormatted}</DateSelected>
+          <ButtonOpacity
+            onPress={() => setDate(addDays(date, 1))}
+            icon="chevron-right"
+            iconSize={40}
+          />
+        </SelectDateContainer>
+
+        <Content>
+          {loading ? (
+            <LoadingScreen />
+          ) : (
+            <MeetupList data={meetups} fieldKey="id" />
+          )}
+        </Content>
+      </Container>
+    </Background>
   );
 }
 
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding-top: 30px;
+`;
+
+export const SelectDateContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const DateSelected = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  margin: 0 15px;
+`;
+
+export const Content = styled.View`
+  padding-top: 30px;
+`;
